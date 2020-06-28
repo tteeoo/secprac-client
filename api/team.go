1fbb916fa8a6679f519ca01422152fac30bc86c8
Theo Henson <theodorehenson@protonmail.com>
2020-06-28 13:02:41 -0400

    Shift functionality from main to api

--- api/team.go
+++ api/team.go
@@ -1,12 +1,51 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
-// CreateTeam interacts with the API to create a new team at the given remote server with the specified token
-func CreateTeam(remote, token string) (*http.Response, error) {
+// Team represents a participating team authenticated on the server
+type Team struct {
+	Token string `json:"token"`
+	ID    string `json:"id"`
+}
+
+// NewTeam interacts with the API to create a new team at the given remote server with the specified token
+func NewTeam(remote, token string) (*Team, error) {
+
+	// Send POST request
 	resp, err := http.Post(remote+"/api/team/create", "application/json", strings.NewReader("{\"token\": \""+token+"\"}"))
-	return resp, err
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("server responded with bad status code: " + strconv.Itoa(resp.StatusCode))
+	}
+
+	// Read response data
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse JSON into map
+	var idJSON map[string]string
+	err = json.Unmarshal(body, &idJSON)
+	if err != nil {
+		return nil, err
+	}
+	if _, ok := idJSON["id"]; !ok {
+		return nil, errors.New("server response JSON does not contain key \"id\"")
+	}
+
+	return &Team{
+		Token: token,
+		ID:    idJSON["id"],
+	}, nil
 }
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
+	"os"
+
 	"github.com/blueberry-jam/secprac-client/api"
 	"github.com/blueberry-jam/secprac-client/util"
-	"io/ioutil"
-	"os"
 )
 
+// Entry point
 func main() {
 
 	// Get remote server from command line args
@@ -29,39 +29,13 @@ func main() {
 	util.Logger.Println("generated team token:", token)
 
 	// Authenticate with the server
-	// Send post req
 	util.Logger.Println("attempting to authenticate with server (" + remote + ")")
-	resp, err := api.CreateTeam(remote, token)
+	team, err := api.NewTeam(remote, token)
 	if err != nil {
 		util.Notify("error", "failed to authenticate with the server, check the log at: "+util.LogFileName, "", true)
 		util.Logger.Fatalln("error authenticating with server:", err)
 	}
-	if resp.StatusCode != 200 {
-		util.Notify("error", "failed to authenticate with the server, check the log at: "+util.LogFileName, "", true)
-		util.Logger.Fatalln("error server responded with HTTP code", resp.Status)
-	}
 
-	// Read response data
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		util.Notify("error", "failed to authenticate with the server, check the log at: "+util.LogFileName, "", true)
-		util.Logger.Fatalln("error reading server response body:", err)
-	}
-
-	// Parse JSON into map
-	var idJSON map[string]string
-	err = json.Unmarshal(body, &idJSON)
-	if err != nil {
-		util.Notify("error", "failed to authenticate with the server, check the log at: "+util.LogFileName, "", true)
-		util.Logger.Fatalln("error parsing server response body JSON:", err)
-	}
-	if _, ok := idJSON["id"]; !ok {
-		util.Notify("error", "failed to authenticate with the server, check the log at: "+util.LogFileName, "", true)
-		util.Logger.Fatalln("server response body JSON did not contain key 'ID'")
-	}
-
-	teamID := idJSON["id"]
-	util.Notify("authenticated", "successfully authenticated with server, your team ID is: "+teamID, "", false)
-	util.Logger.Println("authenticated with", remote, ", given ID", teamID)
+	util.Notify("authenticated", "successfully authenticated with server, your team ID is: "+team.ID, "", false)
+	util.Logger.Println("authenticated with", remote, "given ID", team.ID)
 }
