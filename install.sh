a80f4720f3fd14d97b102298eec89f3202a2c49c
Theo Henson <theodorehenson@protonmail.com>
2020-06-28 23:52:44 -0400

    img/ -> data/

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /usr/local/share/secprac
-cp img/* /usr/local/share/secprac
+cp data/* /usr/local/share/secprac
 
 # will download latest release instead of compile when we have one
 go install
