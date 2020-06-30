70b1f19c6b51a7373cf05a5e990cfb86d6357bff
Theo Henson <theodorehenson@protonmail.com>
2020-06-29 23:18:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
+mkdir -p /usr/local/bin
 mkdir -p /var/log/secprac
 mkdir -p /usr/local/share/secprac
-cp data/* /usr/local/share/secprac
-
-# will download latest release instead of compile when we have one
-go install
+mkdir -p /usr/local/lib/systemd/system
+cd /usr/local/share/secprac
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-logo.png
+cd /usr/local/bin
+wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.1/secprac-client
