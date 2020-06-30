5d9e20ccc4e1642d8b4f7b1c3845324696a529ac
Theo Henson <theodorehenson@protonmail.com>
2020-06-29 23:32:26 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-logo.png
 cd /usr/local/bin
 wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.1/secprac-client
+chmod +x secprac-client
