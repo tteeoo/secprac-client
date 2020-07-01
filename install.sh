f6b458c7bd60d7dcebc24fcf75dcac2a61f1a90c
Theo Henson <theodorehenson@protonmail.com>
2020-07-01 02:47:32 -0400

    Condense and improve install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
-#!/bin/sh
-mkdir -p /usr/local/bin
-mkdir -p /var/log/secprac
-mkdir -p /etc/systemd/system
-mkdir -p /usr/local/share/secprac
-cd /usr/local/bin
-rm secprac-client secprac-start
-wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.4/secprac-client
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-start
-chmod +x secprac-client secprac-start
-cd /usr/local/share/secprac
-rm secprac-plus.png secprac-minus.png secprac-info.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
+mkdir -p /usr/local/bin /var/log/secprac /etc/systemd/system /usr/local/share/secprac
 cd /etc/systemd/system
 rm secprac-client@.service
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-client@.service
+cd /usr/local/bin
+rm secprac-client secprac-start
+wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.4/secprac-client https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-start
+chmod +x secprac-client secprac-start
+cd /usr/local/share/secprac
+rm secprac-plus.png secprac-minus.png secprac-info.png team
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
