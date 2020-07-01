076639314e9cf65a2a4d35e63ced04a845fd3744
Theo Henson <theodorehenson@protonmail.com>
2020-06-30 22:47:53 -0400

    Create secprac-start script and improve install script

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 mkdir -p /usr/local/bin
 mkdir -p /var/log/secprac
+mkdir -p /etc/systemd/system
 mkdir -p /usr/local/share/secprac
-mkdir -p /usr/local/lib/systemd/system
 cd /usr/local/bin
-rm secprac-client
+rm secprac-client secprac-start
 wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.4/secprac-client
-chmod +x secprac-client
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-start
+chmod +x secprac-client secprac-start
 cd /usr/local/share/secprac
 rm secprac-plus.png secprac-minus.png secprac-info.png
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
-cd /usr/local/lib/systemd/system
+cd /etc/systemd/system
 rm secprac-client@.service
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-client@.service
