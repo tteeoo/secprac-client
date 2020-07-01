c9ca253fd3440e3258ef1464383b90bbc2fe8e8e
Theo Henson <theodorehenson@protonmail.com>
2020-06-30 22:00:45 -0400

    Create systemd service file

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 mkdir -p /usr/local/bin
 mkdir -p /var/log/secprac
 mkdir -p /usr/local/share/secprac
-cd /usr/local/share/secprac
-rm secprac-plus.png secprac-minus.png secprac-info.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
-wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png
+mkdir -p /usr/local/lib/systemd/system
 cd /usr/local/bin
 rm secprac-client
 wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.4/secprac-client
 chmod +x secprac-client
+cd /usr/local/share/secprac
+rm secprac-plus.png secprac-minus.png secprac-info.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-info.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-plus.png
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-minus.png
+cd /usr/local/lib/systemd/system
+rm secprac-client@.service
+wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-client@.service
