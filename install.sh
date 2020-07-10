cde3dd834e59630dfaee98889fcb2b1cf53a7d2c
Theo Henson <theodorehenson@protonmail.com>
2020-07-10 16:47:31 -0400

    Bump version

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm secprac-client@.service
 wget https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-client@.service
 cd /usr/local/bin
 rm secprac-client secprac-start
-wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.4/secprac-client https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-start
+wget https://github.com/blueberry-jam/secprac-client/releases/download/0.1.5/secprac-client https://raw.githubusercontent.com/blueberry-jam/secprac-client/master/data/secprac-start
 chmod +x secprac-client secprac-start
 cd /usr/local/share/secprac
 rm secprac-plus.png secprac-minus.png secprac-info.png team
